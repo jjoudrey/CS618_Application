9b1206d1a54d9683e13c8530c04caeb29e7f3b00
Jim Joudrey <jjoudrey@gmail.com>
2025-09-16 10:30:14 +0000

    build: end of chapter 4

--- backend/src/example.js
+++ backend/src/example.js
@@ -1,17 +1,21 @@
 import { initDatabase } from "./db/init.js";
 import { Post } from "./db/models/post.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 await initDatabase();
 const post = new Post({
-  title: "Hello Mongoose!",
-  author: "Daniel Bugl",
-  contents: "This post is stored in a MongoDB database using Mongoose.",
-  tags: ["mongoose", "mongodb"],
+  title: "My Post",
+  author: "Jim Joudrey",
+  contents: "My content",
+  tags: ["frontend"],
 });
 const createdPost = await post.save();
 
 await Post.findByIdAndUpdate(createdPost._id, {
-  $set: { title: 'Hello again, Mongoose!' },
-})
+  $set: { title: "Hello again, Mongoose!" },
+});
 
 const posts = await Post.find();
 console.log(posts);
